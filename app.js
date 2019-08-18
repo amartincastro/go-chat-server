9bb6a1cfa297acba09a5de04438690452ef45b61
amartincastro <castroarielm@gmail.com>
2019-08-17 20:25:37 -0400

    first

---
+++ app.js
@@ -0,0 +1,63 @@
+new Vue({ // Mount vue object to a div with the id of #app
+    el: '#app',
+
+    data: {
+        ws: null, // Our websocket
+        newMsg: '', // Holds new messages to be sent to the server
+        chatContent: '', // A running list of chat messages displayed on the screen
+        email: null, // Email address used for grabbing an avatar
+        username: null, // Our username
+        joined: false // True if email and username have been filled in
+    },
+
+    created: function() { // Vue function initializes when the app launches
+        var self = this;
+        this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+        this.ws.addEventListener('message', function(e) { // Takes a function to handle incoming messages as JSON string and parse them as object literals
+            var msg = JSON.parse(e.data);
+            self.chatContent += '<div class="chip">'
+                    + '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
+                    + msg.username
+                + '</div>'
+                + emojione.toImage(msg.message) + '<br/>'; // Parse emojis using EmojiOne library
+
+            var element = document.getElementById('chat-messages');
+            element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
+        });
+    },
+
+
+    methods: { // Defines the functions we want our app to have access to in the scope of the Vue app
+        send: function () { // Function for sending messages to server
+            if (this.newMsg != '') { // Make sure message isn't blank
+                this.ws.send( 
+                    JSON.stringify({ // Format message as object and stringify it
+                        email: this.email,
+                        username: this.username,
+                        message: $('<p>').html(this.newMsg).text(), // Strip out html to prevent injection
+                        time: this.time,
+                    }
+                ));
+                this.newMsg = ''; // Reset newMsg
+            }
+        },
+
+        join: function () { // Users joining server must enter credentials 
+            if (!this.email) {
+                Materialize.toast('You must enter an email', 2000);
+                return
+            }
+            if (!this.username) {
+                Materialize.toast('You must choose a username', 2000);
+                return
+            }
+            this.email = $('<p>').html(this.email).text(); // Strip out html 
+            this.username = $('<p>').html(this.username).text();
+            this.joined = true; // Set joined to true if above conditions are met
+        },
+
+        gravatarURL: function(email) { // Grabs avatar using user's email address
+            return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email); // One-way encryption to protect user email address submissions
+        }
+    }
+});
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+// Todo list: 
+// Timestamp of messages sent to server and logs
+// Timestamp of messages sent to other users
+// 
+
+import (
+	"log"
+	"net/http"
+	"time"
+	"ioutil"
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)		// Connected clients. The first variable is a map where the key is actually a pointer to a WebSocket. This data structure contains all connected clients to the server.
+
+var broadcast = make(chan Message)         			 // Croadcast channel. This variable is a channel that will act as a queue for messages sent by clients. 
+
+var upgrader = websocket.Upgrader{} 				 // Configure the upgrader - an object with methods for taking a normal HTTP connection and upgrading it to a WebSocket as we'll see later in the code.
+
+type Message struct {
+	Email    string `json:"email"`					 // Define our message object
+	Username string `json:"username"`
+	Message  string `json:"message"`
+	Timestamp int time.Now()
+}
+
+
+
+
+func main() {
+    fs := http.FileServer(http.Dir("../public"))	// Create a file server to route web visitors to our JS app
+    http.Handle("/", fs)
+
+	http.HandleFunc("/ws", handleConnections)		// Configure websocket route to handle any requests for initializing a websocket, and pass in handleConnections function as argument
+
+	go handleMessages() 					// Initialize goroutine for incoming chat messages
+
+	log.Println("http server started on :8000")		// Start the server on localhost port 8000 and log any errors. ---- Add function here to be able to modify port # ----
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	func handleConnections(w http.ResponseWriter, r *http.Request) { // Upgrade initial GET request from cleints into websockets. 
+        ws, err := upgrader.Upgrade(w, r, nil)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+		defer ws.Close()							// Make sure we close the connection when the function returns
+		
+		clients[ws] = true							// Register new clients to the map "clients"
+		
+		for {										// Infinite loop that continuously waits for a new message to be written to the WebSocket
+
+			var msg Message
+			err := ws.ReadJSON(&msg)				// Read in a new message as JSON and map it to a Message object
+			if err != nil {
+					log.Printf("error: %v", err)
+					delete(clients, ws)				// If there is an error from the WebSocket, log the error and remove that client from our global "clients" map so we don't try to read from or send new messages to that client
+					break
+			}
+			broadcast <- msg						// Send the new message to the broadcast channel
+		}
+	}
+
+	func handleMessages() {
+        	for {										 // Grab the next message from the broadcast channel
+                msg := <- broadcast
+                for client := range clients {		 // Send msg to connected clients
+
+                        err := client.WriteJSON(msg)
+                        if err != nil {
+                                log.Printf("error: %v", err)
+                                client.Close()
+                                delete(clients, client)
+                        }
+                }
+        	}
+	}
+
+	func (m Message) saveToFile(filename string) error { // Save messages to local log file
+		return ioutil.WriteFile(filename, []byte(m.toString()), 0666)
+	}
+
+	Message.saveToFile("chat_logs")
+
+}
